95d561c34d369ce787f6a5ad6017d9a432f2617f
Michal Dobrogost <michal.dobrogost@gmail.com>
2024-02-13 13:44:03 -0500

    Convert tabs to spaces.

--- tools/cj-gen-urbcsp/urbcsp.c
+++ tools/cj-gen-urbcsp/urbcsp.c
@@ -16,7 +16,7 @@ void AddNogood(int val1, int val2);
   1. A main() function, which can be used to demonstrate MakeURBCSP().
   2. MakeURBCSP().
   3. ran2(), a random number generator.
-  4. The four functions StartCSP(), AddConstraint(), AddNogood(), and 
+  4. The four functions StartCSP(), AddConstraint(), AddNogood(), and
      EndCSP(), which are called by MakeURBCSP().  The versions
      of these functions given here print out each instance, listing
      the incompatible value pairs of each constraint.  You will need
@@ -38,10 +38,10 @@ int main(int argc, char* argv[])
   if (argc != 7)
     {
       printf("usage: urbcsp #vars #vals #constraints #nogoods seed "
-	     "instances\n");
+             "instances\n");
       return 0;
     }
-	
+
   N = atoi(argv[1]);
   D = atoi(argv[2]);
   C = atoi(argv[3]);
@@ -63,10 +63,10 @@ int main(int argc, char* argv[])
 
 /*********************************************************************
   2. MakeURBCSP() creates a uniform binary constraint satisfaction
-     problem with a specified number of variables, domain size, 
+     problem with a specified number of variables, domain size,
      tightness, and number of constraints.  MakeURBCSP() calls
-     four functions, StartCSP(), AddConstraint(), AddNogood(), and 
-     EndCSP(), which actually create the CSP (that is, build a data 
+     four functions, StartCSP(), AddConstraint(), AddNogood(), and
+     EndCSP(), which actually create the CSP (that is, build a data
      structure).  Feel free to change the signatures of these functions.
      Note that numbering starts from 0: the variables are numbered 0..N-1,
      and the values are numbered 0..K-1.
@@ -76,7 +76,7 @@ int main(int argc, char* argv[])
    D: size of each variable's domain
    C: number of constraints
    T: number of incompatible value pairs in each constraint
-   Seed: a negative number means start a new sequence of 
+   Seed: a negative number means start a new sequence of
       pseudo-random numbers; a positive number means continue
       with the same sequence.  S is turned positive by ran2().
   RETURN VALUE:
@@ -120,15 +120,15 @@ int MakeURBCSP(int N, int D, int C, int T, long *Seed)
     ++instance;       /* increment static variable */
 
   StartCSP(N, D, instance);
-  
+
   /* The program has to choose randomly and uniformly m values from
      n possibilities.  It uses the following logic for both constraints
      and nogood value pairs:
            1. Let t[] be an array of the n possibilities
-	   2. for i = 0 to m-1
-	   3.    r = random(i, n-1)    ; random() returns an int in [i,n-1]
-	   4.    swap t[i] and t[r]
-	   5. end-for
+           2. for i = 0 to m-1
+           3.    r = random(i, n-1)    ; random() returns an int in [i,n-1]
+           4.    swap t[i] and t[r]
+           5. end-for
      At the end of the for loop, the elements from t[0] to t[m-1] are
      the m randomly selected elements.
    */
@@ -152,7 +152,7 @@ int MakeURBCSP(int N, int D, int C, int T, long *Seed)
   for (c=0; c<C; ++c)
     {
       /* Choose a random number between c and PossibleCTs - 1, inclusive. */
-      r =  c + (int) (ran2(Seed) * (PossibleCTs - c)); 
+      r =  c + (int) (ran2(Seed) * (PossibleCTs - c));
 
       /* Swap elements [c] and [r]. */
       selectedCT = CTarray[r];
@@ -166,22 +166,22 @@ int MakeURBCSP(int N, int D, int C, int T, long *Seed)
 
       /* Initialize the NGarray. */
       for (i=0; i<(D*D); ++i)
-	NGarray[i] = i;
+        NGarray[i] = i;
 
       /* Select T incompatible pairs. */
       for (t=0; t<T; ++t)
-	{
-	  /* Choose a random number between t and PossibleNGs - 1, inclusive.*/
-	  r =  t + (int) (ran2(Seed) * (PossibleNGs - t));
-	  selectedNG = NGarray[r];
-	  NGarray[r] = NGarray[t];
-	  NGarray[t] = selectedNG;
+        {
+          /* Choose a random number between t and PossibleNGs - 1, inclusive.*/
+          r =  t + (int) (ran2(Seed) * (PossibleNGs - t));
+          selectedNG = NGarray[r];
+          NGarray[r] = NGarray[t];
+          NGarray[t] = selectedNG;
 
-	  /* Broadcast the nogood value pair. */
-	  AddNogood((int)(NGarray[t] / D), (int)(NGarray[t] % D));
-	}
+          /* Broadcast the nogood value pair. */
+          AddNogood((int)(NGarray[t] / D), (int)(NGarray[t] % D));
+        }
     }
-      
+
   EndCSP();
   free(CTarray);
   free(NGarray);
@@ -192,7 +192,7 @@ int MakeURBCSP(int N, int D, int C, int T, long *Seed)
 
 /*********************************************************************
   3. This random number generator is from William H. Press, et al.,
-     _Numerical Recipes in C_, Second Ed. with corrections (1994), 
+     _Numerical Recipes in C_, Second Ed. with corrections (1994),
      p. 282.  This excellent book is available through the
      WWW at http://nr.harvard.edu/nr/bookc.html.
      The specific section concerning ran2, Section 7.1, is in
@@ -240,11 +240,11 @@ float ran2(long *idum)
       if (*idum < 0)
         *idum += IM1;
       if (j < NTAB)
-	iv[j] = *idum;
+        iv[j] = *idum;
     }
     iy = iv[0];
   }
-      
+
   k = (*idum) / IQ1;
   *idum = IA1 * (*idum - k*IQ1) - k*IR1;
   if (*idum < 0)
@@ -266,17 +266,17 @@ float ran2(long *idum)
 
 
 /*********************************************************************
-  4. An implementation of StartCSP, AddConstraint, AddNogood, and EndCSP 
+  4. An implementation of StartCSP, AddConstraint, AddNogood, and EndCSP
      which prints out the CSP, just listing incompatible value pairs.
      Each constraint starts one a new line, and the id-numbers of the
      variables appear before the colon.  For instance, the output of
         urbcsp 10 5 4 3 9999 10
      begins
         Instance 0
-          8   9: (1 1) (4 0) (0 4) 
-          2   4: (0 3) (3 1) (4 0) 
-          6   9: (4 1) (2 0) (0 3) 
-          1   5: (0 3) (4 0) (0 0) 
+          8   9: (1 1) (4 0) (0 4)
+          2   4: (0 3) (3 1) (4 0)
+          6   9: (4 1) (2 0) (0 3)
+          1   5: (0 3) (4 0) (0 0)
 *********************************************************************/
 
 void StartCSP(int N, int D, int instance)
