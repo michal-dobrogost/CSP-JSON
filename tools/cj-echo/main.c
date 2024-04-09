f3a707d7a56fe2d2908dbdfc0c1d406ba556522b
Michal Dobrogost <michal.dobrogost@gmail.com>
2024-04-09 12:42:28 -0400

    cj-echo: add --normalize option.

--- tools/cj-echo/main.c
+++ tools/cj-echo/main.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <string.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,38 +14,66 @@ void printUsage() {
 
 int main(int argc, char** argv) {
   int err = 0;
-  if (argc != 3) {
+  if (argc != 3 && argc != 4) {
     fprintf(stderr, "ERROR: number of command line parameters.\n\n");
     printUsage();
     return 1;
   }
-  else if (strcmp(argv[1], "--csp") != 0) {
+  bool normalize = false;
+  char* cspInstanceFilename = NULL;
+  for (int iArg = 1; iArg < argc; ) {
+    if (strcmp(argv[iArg], "--normalize") == 0) {
+      normalize = true;
+      iArg++;
+    }
+    else if (strcmp(argv[iArg], "--csp") == 0) {
+      if (iArg >= argc - 1) {
+        fprintf(stderr, "ERROR: --csp flag takes 1 argument.\n\n");
+        printUsage();
+        return 1;
+      }
+      cspInstanceFilename = argv[iArg+1];
+      iArg += 2;
+    }
+    else {
+      fprintf(stderr, "ERROR: unknown argument: %s\n\n", argv[iArg]);
+      printUsage();
+      return 1;
+    }
+  }
+  if (! cspInstanceFilename) {
     fprintf(stderr, "ERROR: missing --csp flag.\n\n");
     printUsage();
     return 1;
   }
-  char* cspInstanceFilename = argv[2];
 
   FILE* cspInstanceFile = fopen(cspInstanceFilename, "r");
   if (! cspInstanceFile) {
-    fprintf(stderr, "ERROR: failed to open csp instance file.\n");
+    fprintf(stderr, "ERROR: failed to open csp instance file: %s\n", cspInstanceFilename);
     return 1;
   }
 
   char* cspJson = NULL;
   size_t cspJsonLen = 0;
-  if (0 != (err = readAll(cspInstanceFile, &cspJson, &cspJsonLen))) {
+  if (CJ_ERROR_OK != (err = readAll(cspInstanceFile, &cspJson, &cspJsonLen))) {
     fprintf(stderr, "ERROR(%d): failed to read csp instance file.", err);
     return 1;
   }
 
   CjCsp csp = cjCspInit();
-  if (0 != (err = cjCspJsonParse(cspJson, cspJsonLen, &csp))) {
+  if (CJ_ERROR_OK != (err = cjCspJsonParse(cspJson, cspJsonLen, &csp))) {
     fprintf(stderr, "ERROR(%d): failed to parse csp instance file.", err);
     return 1;
   }
 
-  if (0 != (err = cjCspJsonPrint(stdout, &csp))) {
+  if (normalize) {
+    if (CJ_ERROR_OK != (err = cjCspNormalize(&csp))) {
+      fprintf(stderr, "ERROR(%d): failed to normalize the csp instance.", err);
+      return 1;
+    }
+  }
+
+  if (CJ_ERROR_OK != (err = cjCspJsonPrint(stdout, &csp))) {
     fprintf(stderr, "ERROR(%d): failed to print CSP.", err);
   }
 
